5381c333ffe3fecba4a914927542b4e1c554342e
Corrosiv <tallowycypress8@gmail.com>
2026-03-25 22:22:27 -0600

    feat: add Scalar API explorer UI and ignore SQLite WAL files

--- FinanceTracker.API/Program.cs
+++ FinanceTracker.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using FinanceTracker.API.Data;
 using FinanceTracker.API.Services;
 
@@ -47,6 +48,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
